f5ba2e305f520c3dc85498a493a6fddd6ecb21c3
Bipul <bipuliitian2016@gmail.com>
2019-06-27 16:55:14 +0530

    added code for login functionality

--- app/src/main/java/com/example/android/mysqldemo/BackgroundTask.java
+++ app/src/main/java/com/example/android/mysqldemo/BackgroundTask.java
@@ -1,12 +1,16 @@
 package com.example.android.mysqldemo;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -16,6 +20,7 @@ import java.net.URLEncoder;
 
 public class BackgroundTask extends AsyncTask<String,Void,String> {
 
+    AlertDialog alertDialog;
     Context ctx;
 
     BackgroundTask(Context context){
@@ -25,11 +30,13 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
     @Override
     protected String doInBackground(String... strings) {
 
-        String reg_url="http://172.20.4.139/webapp/register.php";
-        String login_url="http://172.20.4.139/webapp/login.php";
+        String reg_url="http://172.20.4.156/webapp/register.php";
+        String login_url="http://172.20.4.156/webapp/login.php";
 
         String method=strings[0];
+        Log.d("BackgroundTask : ", "doInbackgorund Invoked !");
         if(method.equals("register")){
+            Log.d("BackgroundTask : ", "Register block Invoked !");
             String name=strings[1];
             String user_name=strings[2];
             String user_pass=strings[3];
@@ -57,7 +64,7 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
                 InputStream inputStream=httpURLConnection.getInputStream();
                 inputStream.close();
 
-                return "registraion successful";
+                return "registration successful";
 
 
             } catch (MalformedURLException e) {
@@ -67,6 +74,53 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
             }
 
         }
+        else if(method.equals("login")){
+            Log.d("BackgroundTask : ", "Inside login block!");
+            String login_name,login_pass;
+            login_name=strings[1];
+            login_pass=strings[2];
+
+            try {
+                URL url=new URL(login_url);
+                HttpURLConnection httpURLConnection= (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setDoInput(true);
+
+                OutputStream os=httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+
+                String data=URLEncoder.encode("login_name", "UTF-8") +"="+URLEncoder.encode(login_name, "UTF-8")+"&"+
+                            URLEncoder.encode("login_pass", "UTF-8")+"="+URLEncoder.encode(login_pass,"UTF-8");
+
+                bufferedWriter.write(data);
+                bufferedWriter.close();
+                os.close();
+
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String response="";
+                String line="";
+                line=bufferedReader.readLine();
+                while (line!=null){
+                    response+=line;
+                    line=bufferedReader.readLine();
+                }
+
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+                return response;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
 
         return "Registration successful";
         //return null;
@@ -74,12 +128,20 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
 
     @Override
     protected void onPreExecute() {
-        super.onPreExecute();
+        alertDialog=new AlertDialog.Builder(ctx).create();
+        alertDialog.setTitle("Login Information .... ");
     }
 
     @Override
     protected void onPostExecute(String result) {
-        Toast.makeText(ctx, result, Toast.LENGTH_LONG).show();
+        if(result.equals("registration successful")) {
+            Toast.makeText(ctx, result, Toast.LENGTH_LONG).show();
+        }
+        else{
+            //Toast.makeText(ctx, result, Toast.LENGTH_LONG).show();
+            alertDialog.setMessage(result);
+            alertDialog.show();
+        }
     }
 
     @Override
--- app/src/main/java/com/example/android/mysqldemo/MainActivity.java
+++ app/src/main/java/com/example/android/mysqldemo/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.android.mysqldemo;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button login,register;
+    EditText Et_Name,Et_pass;
+    String login_name,login_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +22,19 @@ public class MainActivity extends AppCompatActivity {
         login=findViewById(R.id.btn_login);
         register=findViewById(R.id.btn_register);
 
+        Et_Name=findViewById(R.id.et_username);
+        Et_pass=findViewById(R.id.et_password);
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                login_name=Et_Name.getText().toString();
+                login_pass=Et_pass.getText().toString();
+
+                String method="login";
+
+                BackgroundTask backgroundTask=new BackgroundTask(MainActivity.this);
+                backgroundTask.execute(method,login_name,login_pass);
 
             }
         });
